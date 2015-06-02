cad252bee60217e7d95ef4f3654fbcd03c8943fe
AnthonyJMyers <nefarious.myers@gmail.com>
2015-06-02 14:12:19 -0700

    AssignmetnClass

---
+++ Assignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Assignment
+/// </summary>
+public class Assignment
+{
+
+
+    public string classnum { get; set; }
+
+    public string assignment { get; set; }
+
+    public string description { get; set; }
+        
+
+    
+}
